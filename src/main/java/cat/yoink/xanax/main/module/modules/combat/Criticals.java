6df50f8dbb1da82bb7424313d250f645243d818d
Katatje <geertspruijt6@outlook.com>
2020-11-15 15:06:58 +0800

    Remove sample settings

--- src/main/java/cat/yoink/xanax/main/module/modules/combat/Criticals.java
+++ src/main/java/cat/yoink/xanax/main/module/modules/combat/Criticals.java
@@ -3,9 +3,6 @@ package cat.yoink.xanax.main.module.modules.combat;
 import cat.yoink.xanax.main.event.PacketEvent;
 import cat.yoink.xanax.main.module.Category;
 import cat.yoink.xanax.main.module.Module;
-import cat.yoink.xanax.main.setting.BooleanSetting;
-import cat.yoink.xanax.main.setting.EnumSetting;
-import cat.yoink.xanax.main.setting.NumberSetting;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.network.play.client.CPacketUseEntity;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -13,39 +10,10 @@ import org.lwjgl.input.Keyboard;
 
 public final class Criticals extends Module
 {
-    private final BooleanSetting setting1 = addSetting(new BooleanSetting("Test!", true));
-    private final BooleanSetting setting2 = addSetting(new BooleanSetting("Test2", true));
-    private final BooleanSetting setting3 = addSetting(new BooleanSetting("Test3", true));
-    private final BooleanSetting setting4 = addSetting(new BooleanSetting("Test4", true));
-    private final BooleanSetting setting5 = addSetting(new BooleanSetting("5", true));
-    private final BooleanSetting setting6 = addSetting(new BooleanSetting("Test6", true));
-    private final NumberSetting setting62 = addSetting(new NumberSetting("Test!", 5, 2, 8, 0.5));
-    private final EnumSetting setting43 = addSetting(new EnumSetting("Test!", "Two", "One", "Two", "Three"));
-    private final NumberSetting newds = addSetting(new NumberSetting("number", 3.4, 1.1, 9.4, 0.1));
-
     public Criticals()
     {
         super("Criticals", Category.COMBAT);
         setBind(Keyboard.KEY_G);
-
-        addSetting(new NumberSetting("yes", 3, 0, 10, 1));
-        addSetting(new NumberSetting("yes", 5, 0, 10, 1));
-        addSetting(new NumberSetting("yes", 1, 0, 10, 1));
-        addSetting(new NumberSetting("yes", 8, 0, 10, 1));
-        addSetting(new NumberSetting("yes", 7, 0, 10, 1));
-        addSetting(new NumberSetting("yes", 5, 0, 10, 1));
-        addSetting(new NumberSetting("yes", 9, 0, 10, 1));
-        addSetting(new NumberSetting("yes", 10, 0, 10, 1));
-        addSetting(new NumberSetting("yes", 5, 0, 10, 1));
-        addSetting(new NumberSetting("yes", 2, 0, 10, 1));
-        addSetting(new NumberSetting("yes", 6, 0, 10, 1));
-//        addSetting(new NumberSetting("yes", 8, 0, 10, 1));
-    }
-
-    @Override
-    protected void onEnable()
-    {
-//        addSetting(new BooleanSetting("lmfao", false));
     }
 
     @SubscribeEvent
